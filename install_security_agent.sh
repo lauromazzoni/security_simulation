5d1fc5c75d1c17ae20222bdeb337e408ef66e9cd
lauromazzoni <33133685+lauromazzoni@users.noreply.github.com>
2023-12-26 13:57:09 -0300

    Update install_security_agent.sh

--- install_security_agent.sh
+++ install_security_agent.sh
@@ -4,11 +4,7 @@
 echo "Downloading security agent..."
 sudo wget -q https://raw.githubusercontent.com/lauromazzoni/security_simulation/main/security_agent.sh -P /usr/bin
 echo "Security agent installation in progress..."
-echo "1"
 sudo chmod +x /usr/bin/security_agent.sh
-echo "2"
 sleep 5
-echo "3"
 echo "Security agent installation completed."
-echo "4"
 sudo /usr/bin/security_agent.sh status

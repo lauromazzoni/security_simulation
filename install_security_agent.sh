879180e395566188bdd32beac265278e9bd741cc
lauromazzoni <33133685+lauromazzoni@users.noreply.github.com>
2023-12-26 13:34:43 -0300

    Update install_security_agent.sh

--- install_security_agent.sh
+++ install_security_agent.sh
@@ -2,9 +2,9 @@
 # Created by The Cloud Bootcamp - Install Security Agent Simulation
 
 echo "Downloading security agent..."
-sudo wget -q https://raw.githubusercontent.com/lauromazzoni/security_simulation/main/security_simulation.sh -P /usr/bin
+sudo wget -q https://raw.githubusercontent.com/lauromazzoni/security_simulation/main/security_agent.sh -P /usr/bin
 echo "Security agent installation in progress..."
-sudo chmod +x /usr/bin/security_simulation
+sudo chmod +x /usr/bin/security_agent
 sleep 30
 echo "Security agent installation completed."
-sudo /usr/bin/security_simulation status
+sudo /usr/bin/security_agent status
